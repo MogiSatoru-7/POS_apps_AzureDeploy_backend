b679fa4e5bfffeb97015525b8d03ae071413fd90
Mogi Satoru-7 <satoru.mogi.0103@gmail.com>
2024-11-07 22:56:01 +0900

    デプロイエラー対応３

--- database.py
+++ database.py
@@ -8,9 +8,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
-# pymysql を MySQLdb として使用するよう設定
-pymysql.install_as_MySQLdb()
-
 # .envファイルの読み込み
 load_dotenv()
 
@@ -22,23 +19,12 @@ DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 DB_ssl_ca = os.getenv("DB_ssl_ca")
 
-# デバッグ用：環境変数の値を出力
-print(f"DB_USERNAME: {DB_USERNAME}")
-print(f"DB_PASSWORD: {DB_PASSWORD}")
-print(f"DB_HOST: {DB_HOST}")
-print(f"DB_PORT: {DB_PORT}")
-print(f"DB_NAME: {DB_NAME}")
-print(f"DB_ssl_ca: {DB_ssl_ca}")
-
-
 # MySQLデータベースへの接続URL
 # データベース接続URL（Azure 用）
 # DATABASE_URLにSSLオプションを追加
 DATABASE_URL = (
     f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
-
-print("Database URL:", DATABASE_URL)
 
 # SSL 設定を追加してエンジンを作成
 engine = create_engine(
